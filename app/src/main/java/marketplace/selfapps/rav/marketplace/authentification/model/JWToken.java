d24ae42b8c50db0f1d4ea1c1e8cbfb05d7ef08ca
rokhlin <rohlinav@gmail.com>
2017-03-01 04:30:17 +0200

    Add Parcelable in User.java and JWToken.java

--- app/src/main/java/marketplace/selfapps/rav/marketplace/authentification/model/JWToken.java
+++ app/src/main/java/marketplace/selfapps/rav/marketplace/authentification/model/JWToken.java
@@ -1,8 +1,56 @@
 package marketplace.selfapps.rav.marketplace.authentification.model;
 
-/**
- * Created by user on 01.03.2017.
- */
 
-public class JWToken {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class JWToken implements Parcelable {
+    String token;
+
+
+    @Override
+    public String toString() {
+        return token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.token);
+    }
+
+    public JWToken() {
+    }
+
+    public JWToken(String token) {
+        this.token = token;
+    }
+
+    protected JWToken(Parcel in) {
+        this.token = in.readString();
+    }
+
+    public static final Parcelable.Creator<JWToken> CREATOR = new Parcelable.Creator<JWToken>() {
+        @Override
+        public JWToken createFromParcel(Parcel source) {
+            return new JWToken(source);
+        }
+
+        @Override
+        public JWToken[] newArray(int size) {
+            return new JWToken[size];
+        }
+    };
 }
--- app/src/main/java/marketplace/selfapps/rav/marketplace/authentification/model/User.java
+++ app/src/main/java/marketplace/selfapps/rav/marketplace/authentification/model/User.java
@@ -1,17 +1,40 @@
 package marketplace.selfapps.rav.marketplace.authentification.model;
 
-/**
- * Created by user on 01.03.2017.
- */
-
-public class User {
-    String email;
-    String userName;
-    String password;
-    String role;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 
 
+public class User implements Parcelable {
+
+    private String email;
+    private String userName;
+    private String password;
+    private String role;
+
+
+    public User() {
+    }
+
+    public User(String email, String password, String role) {
+        this.email = email;
+
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(String email,String password) {
+        this.email = email;
+        this.password = password;
+
+    }
+
+    protected User(Parcel in) {
+        this.email = in.readString();
+        this.userName = in.readString();
+        this.password = in.readString();
+        this.role = in.readString();
+    }
 
     public String getEmail() {
         return email;
@@ -44,4 +67,31 @@ public class User {
     public void setRole(String role) {
         this.role = role;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.email);
+        dest.writeString(this.userName);
+        dest.writeString(this.password);
+        dest.writeString(this.role);
+    }
+
+
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel source) {
+            return new User(source);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }
