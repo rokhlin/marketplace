99688a2fba740554f7d6589c996af7638ca6be0a
rokhlin <rohlinav@gmail.com>
2017-03-01 04:51:49 +0200

    add into Logs.java new methods receiving Class

--- app/src/main/java/marketplace/selfapps/rav/marketplace/utils/Logs.java
+++ app/src/main/java/marketplace/selfapps/rav/marketplace/utils/Logs.java
@@ -50,4 +50,44 @@ public class Logs {
             else android.util.Log.e(tag, message, e.getCause());
         }
     }
+
+    /**
+     * Send debug level messages
+     * @param clz - need to get Class name for show it in log header
+     * @param message - text which you want to show
+     */
+    public static void log(Class clz, String message){
+        if(logging){
+            String tag = clz.getName();
+            android.util.Log.d(tag, message);
+        }
+    }
+
+
+    /**
+     * Send info level messages
+     * @param clz - need to get Class name for show it in log header
+     * @param message - text which you want to show
+     */
+    public static void logInfo(Class clz,String message){
+        if(logging){
+            String tag = clz.getName();
+            android.util.Log.i(tag, message);
+        }
+    }
+
+    /**
+     * Send error level messages
+     * @param clz - need to get Class name for show it in log header
+     * @param e
+     * @param message - text which you want to show
+     */
+    public static void logError(Class clz, Exception e, String message){
+        if(logging){
+            String tag = clz.getName();
+            if(e == null) android.util.Log.e(tag, message);
+            else if(message == null) android.util.Log.e(tag, e.getMessage(), e.getCause());
+            else android.util.Log.e(tag, message, e.getCause());
+        }
+    }
 }
