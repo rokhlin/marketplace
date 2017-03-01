4a833cbe928f3267ba8ab37073947da91184a3c6
rokhlin <rohlinav@gmail.com>
2017-03-01 04:29:02 +0200

    Created model 	JWToken.java
    	and 	User.java

---
+++ app/src/main/java/marketplace/selfapps/rav/marketplace/authentification/model/JWToken.java
@@ -0,0 +1,8 @@
+package marketplace.selfapps.rav.marketplace.authentification.model;
+
+/**
+ * Created by user on 01.03.2017.
+ */
+
+public class JWToken {
+}
---
+++ app/src/main/java/marketplace/selfapps/rav/marketplace/authentification/model/User.java
@@ -0,0 +1,47 @@
+package marketplace.selfapps.rav.marketplace.authentification.model;
+
+/**
+ * Created by user on 01.03.2017.
+ */
+
+public class User {
+    String email;
+    String userName;
+    String password;
+    String role;
+
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
