70d8335ddf18c9ea9614393fb3025113e6592e09
rokhlin <rohlinav@gmail.com>
2017-03-04 19:50:32 +0200

    add SignOutActivity

---
+++ app/src/main/java/marketplace/selfapps/rav/marketplace/SignOutActivity.java
@@ -0,0 +1,75 @@
+package marketplace.selfapps.rav.marketplace;
+
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.support.v4.os.CancellationSignal;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import marketplace.selfapps.rav.marketplace.authentification.model.JWToken;
+
+import static marketplace.selfapps.rav.marketplace.utils.Logs.*;
+
+public class SignOutActivity extends AppCompatActivity {
+    private static final String TOKEN = "TOKEN" ;
+    private SharedPreferences sPref;
+    private AlertDialog.Builder ad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_out);
+        
+        ad = new AlertDialog.Builder(this);
+        ad.setTitle("Sign Out");  // заголовок
+        ad.setMessage(""); // сообщение
+        ad.setPositiveButton("Sign Out", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int arg1) {
+               log(getClass(),"Sign Out button pressed");
+            }
+        });
+        ad.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int arg1) {
+                log(getClass(),"Cancel button pressed");
+            }
+        });
+        ad.setCancelable(true);
+
+
+        Button signOut = (Button) findViewById(R.id.btn_sign_out);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ad.show();
+            }
+        });
+    }
+
+    /**
+     * Save actual TOKEN in SharedPreferences
+     * @param token of the authentication will saved in  SharedPreferences
+     */
+    public void saveToken(JWToken token) {
+        sPref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = sPref.edit();
+        ed.putString(TOKEN, token.toString());
+        ed.apply();
+        log(SignOutActivity.this," token saved!");
+    }
+
+    /**
+     * Load actual TOKEN from the SharedPreferences if contain
+     * @return token of the authentication will returned from the SharedPreferences
+     */
+    public JWToken loadToken() {
+        sPref = getPreferences(MODE_PRIVATE);
+        String token = sPref.getString(TOKEN, null);
+        if(token!= null)
+            log(SignOutActivity.this," token loaded from preferences!" );
+        return token!= null ? new JWToken(token): null;
+    }
+}
